4171d8e07a297f715795d7ee3fe667fbe549d053
Ycarus <ycarus@zugaina.org>
2018-10-05 17:13:38 +0200

    Choose interface used for each domains, ips, networks or protocols that bypass OMR

--- luci-app-omr-bypass/luasrc/controller/omr-bypass.lua
+++ luci-app-omr-bypass/luasrc/controller/omr-bypass.lua
@@ -4,48 +4,6 @@ module("luci.controller.omr-bypass", package.seeall)
 
 function index()
 	entry({"admin", "services", "omr-bypass"}, alias("admin", "services", "omr-bypass", "index"), _("OMR-Bypass"))
-	entry({"admin", "services", "omr-bypass", "index"}, template("omr-bypass/bypass"))
-	entry({"admin", "services", "omr-bypass", "add"}, post("bypass_add"))
-end
-
-function bypass_add()
-	local hosts = luci.http.formvalue("cbid.omr-bypass.hosts")
-	if (type(hosts) ~= "table") then
-		hosts = {hosts}
-	end
-	local domains_ipset = ""
-	local ip_ipset = {}
-	for _, k in pairs(hosts) do
-		if k ~= "" then
-			if dt.ipmask(k) then
-				table.insert(ip_ipset, k)
-			else
-				domains_ipset = domains_ipset .. '/' .. k
-			end
-		end
-	end
-	ucic:set_list("omr-bypass","ips","ip",ip_ipset)
-
-	local dpi = luci.http.formvalue("cbid.omr-bypass.dpi")
-	if dpi ~= "" then
-		if (type(dpi) ~= "table") then
-			dpi = {dpi}
-		end
-		ucic:set_list("omr-bypass","dpi","proto",dpi)
-	else
-		ucic:delete("omr-bypass","dpi","proto")
-	end
-
-	local interface = luci.http.formvalue("cbid.omr-bypass.interface") or ""
-	ucic:set("omr-bypass","defaults","ifname",interface)
-	
-	ucic:save("omr-bypass")
-	ucic:commit("omr-bypass")
-	ucic:set_list("dhcp",ucic:get_first("dhcp","dnsmasq"),"ipset",domains_ipset .. "/ss_rules_dst_bypass_all")
-	ucic:save("dhcp")
-	ucic:commit("dhcp")
-	luci.sys.exec("/etc/init.d/dnsmasq reload")
-	luci.sys.exec("/etc/init.d/omr-bypass restart")
-	luci.http.redirect(luci.dispatcher.build_url("admin/services/omr-bypass"))
-	return
+	--entry({"admin", "services", "omr-bypass", "index"}, template("omr-bypass/bypass"))
+	entry({"admin", "services", "omr-bypass", "index"}, cbi("omr-bypass"))
 end
---
+++ luci-app-omr-bypass/luasrc/model/cbi/omr-bypass.lua
@@ -0,0 +1,72 @@
+-- Copyright 2018 Ycarus (Yannick Chabanois) <ycarus@zugaina.org>
+-- Licensed to the public under the Apache License 2.0.
+
+local ipc = require "luci.ip"
+local sys = require "luci.sys"
+local net = require "luci.model.network".init()
+local ifaces = net:get_interfaces() or { net:get_interface() }
+
+m = Map("omr-bypass", translate("Bypass"))
+
+s = m:section(TypedSection, "domains", translate("Domains"))
+s.addremove = true
+s.anonymous = true
+s.template = "cbi/tblsection"
+
+hn = s:option(Value, "name", translate("Domain"))
+hn.datatype = "hostname"
+hn.optional = false
+hn.rmempty  = true
+
+ifd = s:option(Value, "interface", translate("Interface"))
+ifd.rmempty  = true
+
+s = m:section(TypedSection, "ip", translate("IPs and Networks"))
+s.addremove = true
+s.anonymous = true
+s.template = "cbi/tblsection"
+
+ip = s:option(Value, "ips", translate("IP"))
+ip.datatype = "ipaddr"
+ip.rmempty  = true
+ip.optional = false
+
+ifi = s:option(Value, "interface", translate("Interface"))
+ifi.rmempty  = true
+
+s = m:section(TypedSection, "dpis", translate("Protocols"))
+s.addremove = true
+s.anonymous = true
+s.template = "cbi/tblsection"
+
+dpi = s:option(Value, "proto", translate("Protocol"))
+dpi.rmempty  = true
+dpi.optional = false
+local protos = {}
+for l in io.lines("/proc/net/xt_ndpi/proto") do
+	local a,b,c,d = l:match('(%w+) (%w+)')
+	if b ~= "2" and not string.match(b,"custom") then
+		table.insert(protos,b)
+	end
+end
+table.sort(protos)
+for _,b in ipairs(protos) do
+	dpi:value(b,"%s" % tostring(b))
+end
+
+ifp = s:option(ListValue, "interface", translate("Interface"))
+ifp.rmempty  = true
+
+ifd.default = "all"
+ifi.default = "all"
+ifp.default = "all"
+ifd:value("all",translate("Master interface"))
+ifi:value("all",translate("Master interface"))
+ifp:value("all",translate("Master interface"))
+for _, iface in ipairs(ifaces) do
+	ifd:value(iface:name(),"%s" % iface:name())
+	ifi:value(iface:name(),"%s" % iface:name())
+	ifp:value(iface:name(),"%s" % iface:name())
+end
+
+return m
