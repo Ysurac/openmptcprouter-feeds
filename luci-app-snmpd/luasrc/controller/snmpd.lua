b3cc953368b4de134a785c982838c05a807e4a88
Ycarus <ycarus@zugaina.org>
2018-11-09 17:35:23 +0100

    Rename SNMPD to SNMPd

--- luci-app-snmpd/luasrc/controller/snmpd.lua
+++ luci-app-snmpd/luasrc/controller/snmpd.lua
@@ -3,6 +3,6 @@ local dt = require "luci.cbi.datatypes"
 module("luci.controller.snmpd", package.seeall)
 
 function index()
-	entry({"admin", "network", "snmpd"}, alias("admin", "network", "snmpd", "index"), _("SNMPD"))
+	entry({"admin", "network", "snmpd"}, alias("admin", "network", "snmpd", "index"), _("SNMPd"))
 	entry({"admin", "network", "snmpd", "index"}, cbi("snmpd"))
 end
