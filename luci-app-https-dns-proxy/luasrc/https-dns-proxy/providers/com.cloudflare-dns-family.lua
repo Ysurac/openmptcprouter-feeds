02317e2caf775014b350ef0953eddd8e8466da04
Ycarus (Yannick Chabanois) <ycarus@zugaina.org>
2020-04-10 09:58:19 +0200

    Add luci-app-https-dns-proxy

---
+++ luci-app-https-dns-proxy/luasrc/https-dns-proxy/providers/com.cloudflare-dns-family.lua
@@ -0,0 +1,6 @@
+return {
+	name = "Cloudflare for Families",
+	label = _("Cloudflare for Families"),
+	resolver_url = "https://family.cloudflare-dns.com/dns-query",
+	bootstrap_dns = "1.1.1.3,1.0.0.3"
+}
