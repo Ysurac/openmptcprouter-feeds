4a825c5650fffe9fddce96c264fefac6152f3cd9
Ycarus (Yannick Chabanois) <ycarus@zugaina.org>
2020-12-03 11:22:39 +0100

    New interface for mlvpn VPN

--- luci-app-mlvpn/luasrc/controller/mlvpn.lua
+++
@@ -1,7 +0,0 @@
-module("luci.controller.mlvpn", package.seeall)
-
-function index()
-	--entry({"admin", "openmptcprouter", "mlvpn"}, cbi("mlvpn"), _("MLVPN"))
-	--entry({"admin", "services", "mlvpn"}, cbi("mlvpn"), _("MLVPN"))
-	entry({"admin", "vpn", "mlvpn"}, cbi("mlvpn"), _("MLVPN"))
-end
