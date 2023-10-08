dd13942d683c4a836461223ae2346d9af68c69c3
suyuan <175338101@qq.com>
2023-10-08 15:15:19 +0800

    Sync to latest

--- luci-app-ouad/root/usr/sbin/status_page2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-echo "router: $(uci -q get openmptcprouter.settings.version)"
-echo "server: $(uci -q get openmptcprouter.vps.omr_version)"
-echo "uptime: $(uptime | awk -F, '{sub(".*up ",x,$1);print $1,$2}' | sed 's/  */ /g')"
-echo "temp: $(awk '{printf("%.1f°C",$1/1e3)}' /sys/class/thermal/thermal_zone0/temp)"
