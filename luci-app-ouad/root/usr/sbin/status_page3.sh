7b3188c7ced7bb8b3e0747b0a512d34b4c75b549
suyuan <175338101@qq.com>
2023-10-22 02:44:51 +0800

    Fix  oled syntax errors

--- luci-app-ouad/root/usr/sbin/status_page3.sh
+++ luci-app-ouad/root/usr/sbin/status_page3.sh
@@ -4,11 +4,11 @@ routerid=$(uci -q get rtty.@rtty[0].id | cut -c-9)
 vpsip=$(curl https://55860.com/modules/addons/flowpacket/api.php?routeid=${routerid} | jsonfilter -e '@.data.dedicatedip')
 [ -n "$vpsip" ] && {
 	vpsdata=$(curl https://55860.com/modules/addons/flowpacket/getWay.php?routeid=${vpsip})
-	user=$(echo ${vpsdata} | jsonfilter -r '@.email')
-	data=$(echo ${vpsdata} | jsonfilter -r '@.data')
-	nextduedate=$(echo ${vpsdata} | jsonfilter -r '@.nextduedate')
+	user=$(echo ${vpsdata} | jsonfilter -e '@.email')
+	data=$(echo ${vpsdata} | jsonfilter -e '@.data')
+	nextduedate=$(echo ${vpsdata} | jsonfilter -e '@.nextduedate')
 	echo "user: ${user}"
-	echo "residual flow: ${data}
-	echo "router ID: ${routerid}
+	echo "residual flow: ${data}"
+	echo "router ID: ${routerid}"
 	echo "next due date: ${nextduedate}"
 }
