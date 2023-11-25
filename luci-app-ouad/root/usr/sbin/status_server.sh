2e464fefde0f5e2d78a37dfe3a72358079c3c1d8
suyuan <175338101@qq.com>
2023-11-25 14:34:53 +0800

    fix update

--- luci-app-ouad/root/usr/sbin/status_server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-public_ip=$(uci -q get openmptcprouter.omr.detected_public_ipv4 | tr -d "\n")
-if [ -n "$public_ip" ]; then
-    echo "IP: ${public_ip}"
-else
-    echo "Waiting for server..."
-fi
