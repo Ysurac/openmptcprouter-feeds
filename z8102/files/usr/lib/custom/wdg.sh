9d0985b6e2a6876f9eed91a8c8573858e25c3fe3
Ycarus (Yannick Chabanois) <ycarus@zugaina.org>
2024-04-25 13:54:28 +0200

    Fix z8102 watchdog

--- z8102/files/usr/lib/custom/wdg.sh
+++ z8102/files/usr/lib/custom/wdg.sh
@@ -7,9 +7,11 @@
 while true
 do
 	#echo "1" > /sys/class/gpio/gpio${i}/value
-	gpioset `gpiofind "watchdog"`=1
+	gpioset `gpiofind "watchdog"`=1 2>&1 >/dev/null
+	gpioset --hold-period 100ms -t0 watchdog=1 2>&1 >/dev/null
 	sleep 1
 	#echo "0" > /sys/class/gpio/gpio${i}/value
-	gpioset `gpiofind "watchdog"`=0
+	gpioset `gpiofind "watchdog"`=0 2>&1 >/dev/null
+	gpioset --hold-period 100ms -t0 watchdog=0 2>&1 >/dev/null
 	sleep 1
 done
