31ed3b3b7b37b10d0f2ad421fabf5fceeab07814
Ycarus (Yannick Chabanois) <ycarus@zugaina.org>
2024-08-10 20:43:44 +0200

    Fix luci-app-mptcp for 5.4

--- luci-app-mptcp/luasrc/controller/mptcp.lua
+++ luci-app-mptcp/luasrc/controller/mptcp.lua
@@ -9,7 +9,11 @@ module("luci.controller.mptcp", package.seeall)
 function index()
 	local uname = nixio.uname()
 	entry({"admin", "network", "mptcp"}, alias("admin", "network", "mptcp", "settings"), _("MPTCP"))
-	entry({"admin", "network", "mptcp", "settings"}, view("mptcp/mptcp"), _("Settings"),2).leaf = true
+	if uname ~= nil and uname.release:sub(1,1) == "5" then
+		entry({"admin", "network", "mptcp", "settings"}, cbi("mptcp"), _("Settings"),2).leaf = true
+	else
+		entry({"admin", "network", "mptcp", "settings"}, view("mptcp/mptcp"), _("Settings"),2).leaf = true
+	end
 	entry({"admin", "network", "mptcp", "bandwidth"}, template("mptcp/multipath"), _("Bandwidth"), 3).leaf = true
 	entry({"admin", "network", "mptcp", "multipath_bandwidth"}, call("multipath_bandwidth")).leaf = true
 	entry({"admin", "network", "mptcp", "interface_bandwidth"}, call("interface_bandwidth")).leaf = true
