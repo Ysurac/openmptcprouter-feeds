8e1a2e2dc7eb0318c8d1cde965aed72cedac797d
Ycarus (Yannick Chabanois) <ycarus@zugaina.org>
2024-12-06 11:03:16 +0100

    Remove https-dns-proxy

--- luci-app-https-dns-proxy/luasrc/https-dns-proxy/providers/com.cloudflare-dns-family.lua
+++
@@ -1,6 +0,0 @@
-return {
-	name = "Cloudflare for Families",
-	label = _("Cloudflare for Families"),
-	resolver_url = "https://family.cloudflare-dns.com/dns-query",
-	bootstrap_dns = "1.1.1.3,1.0.0.3"
-}
